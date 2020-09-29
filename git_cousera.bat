533585dff61fa80588ccdb6b9d0a8d548ea1fa9f
m4theussouza@gmail.com <m4theussouza@gmail.com>
2020-09-29 09:19:08 -0300

    Python tips

--- git_cousera.bat
+++ git_cousera.bat
@@ -1,7 +1,5 @@
-git init
 git add .
 SET /P commit=[Digite o nome do commit:]
 git commit -m "%commit%"
-git branch -M master
-git remote add origin git@github.com:m4theussouza/Introduction-to-Data-Sciense-in-Python.git
+git pull origin master
 git push -u origin master
